e17d7b5502d0a0dfc3452c48869e818270681600
Adriel da Silva <silvaadriel510@gmail.com>
2019-11-28 16:05:55 -0300

    Create username input view

--- src/app/Characters/views/Characters.view.vue
+++ src/app/Characters/views/Characters.view.vue
@@ -1,7 +1,36 @@
 <template>
   <div class="flex justify-center column text-center">
-    <user-profile-header/>
-    <characters-grid/>
+    <v-container v-if="!username" fill-height>
+      <v-layout class="align-center justify-center">
+        <v-card
+          raised
+          height="200"
+          width="400"
+          elevation="20"
+          class="text-center"
+        >
+          <v-card-title>Nome de usuário</v-card-title>
+          <div class="px-2">
+            <v-text-field
+              ref="textField"
+              label="Digite seu nome"
+              outlined
+            ></v-text-field>
+          </div>
+          <v-card-actions>
+            <v-btn
+              block
+              color="primary"
+              @click="setUsername"
+            >
+              Entrar
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-layout>
+    </v-container>
+    <user-profile-header v-if="username"/>
+    <characters-grid v-if="username"/>
   </div>
 </template>
 
@@ -15,5 +44,20 @@ export default {
     UserProfileHeader,
     CharactersGrid,
   },
+  data: () => ({
+    username: '',
+  }),
+  methods: {
+    setUsername() {
+      const { textField } = this.$refs;
+      this.username = textField.$refs.input.value;
+    },
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+.fill-height {
+  height: 100vh;
+}
+</style>
